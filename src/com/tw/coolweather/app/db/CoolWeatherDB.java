9ece117f7f41cd5bff7780803bb41e13e4af53db
Kerry <kerry50407@gmail.com>
2014-10-29 18:14:57 +0800

    add chooseAreaActivity

--- src/com/tw/coolweather/app/db/CoolWeatherDB.java
+++ src/com/tw/coolweather/app/db/CoolWeatherDB.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.tw.coolweather.app.model.City;
+import com.tw.coolweather.app.model.County;
 import com.tw.coolweather.app.model.Province;
 
 public class CoolWeatherDB {
@@ -20,19 +21,20 @@ public class CoolWeatherDB {
 	 * 資料庫版本
 	 */
 	public static final int VERSION = 1;
-	
+
 	private static CoolWeatherDB coolWeatherDB;
-	
+
 	private SQLiteDatabase db;
-	
+
 	/**
 	 * 將構造方法私有化
 	 */
 	private CoolWeatherDB(Context context) {
-		CoolWeatherOpenHelper dbHelper = new CoolWeatherOpenHelper(context, DB_NAME, null, VERSION);
+		CoolWeatherOpenHelper dbHelper = new CoolWeatherOpenHelper(context,
+				DB_NAME, null, VERSION);
 		db = dbHelper.getWritableDatabase();
 	}
-	
+
 	/**
 	 * 獲取CoolWeatherDB的實例
 	 */
@@ -42,42 +44,45 @@ public class CoolWeatherDB {
 		}
 		return coolWeatherDB;
 	}
-	
+
 	/**
 	 * 將Province實例儲存到資料庫
 	 */
 	public void saveProvince(Province province) {
-		if(province != null) {
+		if (province != null) {
 			ContentValues values = new ContentValues();
 			values.put("province_name", province.getProvinceName());
 			values.put("province_code", province.getProvinceCode());
 			db.insert("Province", null, values);
 		}
 	}
-	
+
 	/**
 	 * 從資料庫讀取全國所有的省份信息
 	 */
 	public List<Province> loadProvinces() {
 		List<Province> list = new ArrayList<Province>();
-		Cursor cursor = db.query("Province", null, null, null, null, null, null);
-		if(cursor.moveToFirst()) {
+		Cursor cursor = db
+				.query("Province", null, null, null, null, null, null);
+		if (cursor.moveToFirst()) {
 			do {
 				Province province = new Province();
 				province.setId(cursor.getInt(cursor.getColumnIndex("id")));
-				province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
-				province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+				province.setProvinceName(cursor.getString(cursor
+						.getColumnIndex("province_name")));
+				province.setProvinceCode(cursor.getString(cursor
+						.getColumnIndex("province_code")));
 				list.add(province);
-			} while(cursor.moveToNext());
+			} while (cursor.moveToNext());
 		}
 		return list;
 	}
-	
+
 	/**
 	 * 將City實例儲存到資料庫
 	 */
 	public void saveCity(City city) {
-		if(city != null) {
+		if (city != null) {
 			ContentValues values = new ContentValues();
 			values.put("city_name", city.getCityName());
 			values.put("city_code", city.getCityCode());
@@ -85,22 +90,62 @@ public class CoolWeatherDB {
 			db.insert("City", null, values);
 		}
 	}
-	
+
 	/**
 	 * 從資料庫讀取某省下所有的城市信息
 	 */
 	public List<City> loadCities(int provinceId) {
 		List<City> list = new ArrayList<City>();
-		Cursor cursor = db.query("City", null, "province_id = ?", new String[] { String.valueOf(provinceId) }, null , null, null);
-		if(cursor.moveToFirst()) {
+		Cursor cursor = db.query("City", null, "province_id = ?",
+				new String[] { String.valueOf(provinceId) }, null, null, null);
+		if (cursor.moveToFirst()) {
 			do {
 				City city = new City();
 				city.setId(cursor.getInt(cursor.getColumnIndex("id")));
-				city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
-				city.setCityCode(cursor.getString(cursor.getColumnIndex("city_name")));
+				city.setCityName(cursor.getString(cursor
+						.getColumnIndex("city_name")));
+				city.setCityCode(cursor.getString(cursor
+						.getColumnIndex("city_code")));
 				city.setProvinceId(provinceId);
+				list.add(city);
 			} while (cursor.moveToNext());
 		}
 		return list;
 	}
+
+	/**
+	 * 將County實例儲存到資料庫
+	 */
+	public void saveCounty(County county) {
+		if (county != null) {
+			ContentValues values = new ContentValues();
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			values.put("city_id", county.getCityId());
+			db.insert("County", null, values);
+		}
+	}
+
+	/**
+	 * 從資料庫讀取某城市下的所有信息
+	 */
+	public List<County> loadCounties(int cityId) {
+		List<County> list = new ArrayList<County>();
+		Cursor cursor = db.query("County", null, "city_id = ?",
+				new String[] { String.valueOf(cityId) }, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				County county = new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString(cursor
+						.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString(cursor
+						.getColumnIndex("county_code")));
+				county.setCityId(cityId);
+				list.add(county);
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
+
 }
--- src/com/tw/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/tw/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -11,22 +11,22 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	 * Province表建表語法
 	 */
 	public static final String CREATE_PROVINCE = "create table Province ("
-			+ "id integer primary key autoincrement, " + "city_name text, "
-			+ "city_code text, " + "province_id integer)";
+			+ "id integer primary key autoincrement, " + "province_name text, "
+			+ "province_code text) ";
 
 	/**
 	 *City表建表語法
 	 */
-	public static final String CREATE_CITY = "create table city ("
+	public static final String CREATE_CITY = "create table City ("
 			+ "id integer primary key autoincrement, " + "city_name text, "
 			+ "city_code text," + "province_id integer) ";
 
 	/**
-	 * Country表建表語法
+	 * County表建表語法
 	 */
-	public static final String CREATE_COUNTRY = "create table country ("
-			+ "id integer primary key autoincrement, " + "country_name text,"
-			+ "country_code text," + "city_id integer) ";
+	public static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key autoincrement, " + "county_name text,"
+			+ "county_code text," + "city_id integer) ";
 
 	public CoolWeatherOpenHelper(Context context, String name,
 			CursorFactory factory, int version) {
@@ -37,7 +37,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_PROVINCE);
 		db.execSQL(CREATE_CITY);
-		db.execSQL(CREATE_COUNTRY);
+		db.execSQL(CREATE_COUNTY);
 	}
 
 	@Override
